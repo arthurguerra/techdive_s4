2515908b3127189e90d2a442f27a162b690f5346
Arthur Guerra <arthur_gb1996@hotmail.com>
2021-11-29 08:27:39 -0300

    alteração exercício 6

--- src/modulo1/s4/Ex6.java
+++ src/modulo1/s4/Ex6.java
@@ -1,7 +1,12 @@
 package modulo1.s4;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
@@ -13,22 +18,22 @@ public class Ex6 {
     Se o usuário tiver 18 anos ou mais, escreva na tela “você é maior de idade”,
     mas caso tenha menos de 18 anos, escreva “você é menor de idade”.*/
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Data de nascimento (dia/mês/ano): ");
         String nascimento = sc.nextLine();
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            Date data = sdf.parse(nascimento);
-        } catch (ParseException e) {
-            e.printStackTrace();
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        int ano = Integer.parseInt(nascimento.substring(nascimento.lastIndexOf('/')+1));
+        int idade = Calendar.getInstance().get(Calendar.YEAR) - ano;
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
         }
-        Integer ano = Integer.parseInt(nascimento.substring(nascimento.lastIndexOf('/')+1));
-        Integer idade = Calendar.getInstance().get(Calendar.YEAR) - ano;
-        System.out.println(ano);
-        System.out.println(idade);
     }
 }
